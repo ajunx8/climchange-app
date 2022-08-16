80ed06f3a26010e65fbff4d7af68a4394cd5d449
Adrian Greksa <adrian.greksa@gmail.com>
2022-08-16 13:10:27 +1000

    stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import './index.css'
+import './App.css'
 import { useState, useEffect } from 'react'
 import { supabase } from './supabaseClient'
 import Auth from './Auth'
-import Account from './Account'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import News from './components/News';
 import Home from './pages/Home';
@@ -36,47 +35,3 @@ export default function App() {
     </BrowserRouter> 
   )
 }
-
-
-  // {/* {!session ? <Auth /> : <Account key={session.user.id} session={session} />} */}
-  //     <BrowserRouter>
-  //   <div className="container" style={{ padding: '50px 0 100px 0' }}>
-  //       <Switch>
-  //         <Route index element={<App />}/>
-  //         <Route path="/profile" >
-  //           {!session ? <Auth /> : <News />}
-  //         </Route>
-  //       </Switch>
-  //   </div>
-  //     </BrowserRouter>
-  // )
-
-
-
-//////////////////////////////////////////////
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Blogs = () => {
-    const [content,setContent] = useState('');
+    const [content, setContent] = useState('');
 
     const _handleSubmit = (e) => {
         e.preventDefault();
